f179acce7b6c49ed395e9b80136b17bc9c9a3915
Chris <codingbooth03@gmail.com>
2024-03-06 10:19:22 -0500

    Updated Mobile Menu

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const close = document.getElementById("mobilePopupClose")
 const hamburger = document.getElementById("hamburger")
 
-hamburger.addEventListener("click touchstart", () => {
-    mobilePopup.style.display = "visible";
+const bar1 = document.querySelector(".bar1")
+const bar2 = document.querySelector(".bar2")
+const bar3 = document.querySelector(".bar3")
+
+const mobilePopup = document.getElementById("mobilePopup")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("open")
+    
+    if (hamburger.classList.contains("open")) {
+        mobilePopup.style.visibility = "visible"
+    } else {
+        mobilePopup.style.visibility = "hidden"
+    }
 })
